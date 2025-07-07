5a393c4f057f1ee406ea9c334f55ca3a5c4d9553
Babur Makhmudov <bmuddha13@gmail.com>
2025-07-07 14:23:39 +0400

    feat: add mocked methods for compatibility with solana explorer

--- src/rpc/http.rs
+++ src/rpc/http.rs
@@ -2,6 +2,10 @@ use std::rc::Rc;
 
 use jsonrpsee::{core::RpcResult, proc_macros::rpc};
 use solana_account_decoder::{parse_token::UiTokenAmount, UiAccount};
+use solana_epoch_info::EpochInfo;
+use solana_epoch_schedule::{
+    EpochSchedule, DEFAULT_LEADER_SCHEDULE_SLOT_OFFSET, MINIMUM_SLOTS_PER_EPOCH,
+};
 use solana_rpc_client_api::{
     config::{
         RpcAccountInfoConfig, RpcContextConfig, RpcSendTransactionConfig, RpcTransactionConfig,
@@ -65,6 +69,34 @@ pub trait RoHttpRpc {
 
     #[method(name = "getBlockhashForAccounts")]
     async fn blockhash_for_accounts(&self, accounts: Vec<SerdePubkey>) -> RpcResult<RpcBlockhash>;
+
+    #[method(name = "getFirstAvailableBlock")]
+    async fn first_available_block(&self) -> RpcResult<u64> {
+        RpcResult::Ok(0)
+    }
+
+    #[method(name = "getEpochSchedule")]
+    async fn epoch_schedule(&self) -> RpcResult<EpochSchedule> {
+        RpcResult::Ok(EpochSchedule {
+            slots_per_epoch: MINIMUM_SLOTS_PER_EPOCH,
+            leader_schedule_slot_offset: DEFAULT_LEADER_SCHEDULE_SLOT_OFFSET,
+            warmup: false,
+            first_normal_epoch: 0,
+            first_normal_slot: 0,
+        })
+    }
+
+    #[method(name = "getEpochInfo")]
+    async fn epoch_info(&self) -> RpcResult<EpochInfo> {
+        RpcResult::Ok(EpochInfo {
+            epoch: 0,
+            slot_index: 0,
+            slots_in_epoch: 0,
+            absolute_slot: 0,
+            block_height: 0,
+            transaction_count: None,
+        })
+    }
 }
 
 #[rpc(server)]
