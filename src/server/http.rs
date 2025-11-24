f2f221b22537f30a16d766adf9a69c67b564b080
Gabriele Picco <piccogabriele@gmail.com>
2025-11-24 13:05:02 +0400

    feat: add fqdn to getDelegationStatus (#47)

--- src/server/http.rs
+++ src/server/http.rs
@@ -332,8 +332,20 @@ impl RoHttpRpcServer for HttpServer {
 
     async fn delegation_status(&self, pubkey: SerdePubkey) -> RpcResult<DelegationStatus> {
         let record = self.delegations.get_record(pubkey.0).await;
+        // Determine fqdn based on delegation status
+        let fqdn = if let Some(ref rec) = record {
+            let authority = rec.authority.0;
+            let client = self
+                .routes
+                .ephemeral_client(&authority)
+                .ok_or_else(|| RouterError::UnknownErNode(authority))?;
+            client.url()
+        } else {
+            self.routes.base_chain().client.url()
+        };
         let status = DelegationStatus {
             is_delegated: record.is_some(),
+            fqdn,
             delegation_record: record,
         };
         Ok(status)
